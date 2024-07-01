c5a0da280ac4bf3454608f45a10fbbd6e100a874
Cris-rq <cristinaespejoroque@outlook.es>
2024-07-01 18:33:40 +0200

    feat: starting database with fake animals

--- backend/proteapp/api/main.py
+++ backend/proteapp/api/main.py
@@ -4,11 +4,27 @@ from proteapp.api.routes.treatments import router as treatments_router
 from proteapp.api.deps import get_session
 from proteapp.models.animals import Animal, Sex
 from proteapp.models.treatments import Treatment
-
-
+from proteapp.api.fakes import animales
 def main():
     session_generator = get_session()
     session = next(session_generator)
+
+    # for data in animales:
+    #     animal = Animal(
+    #         name=data["name"], 
+    #         description=data["description"], 
+    #         personality=data["personality"], 
+    #         sex=data["sex"], 
+    #         birthDate=data["birthDate"], 
+    #         entryDate=data["entryDate"], 
+    #         isCastrated=data["isCastrated"], 
+    #         isAnimalCompatible=data["isAnimalCompatible"], 
+    #         yard=data["yard"], 
+    #         hasTreatment=data["hasTreatment"], 
+    #         image=data["image"]
+    #     )
+    #     session.add(animal)
+    #     session.commit()
 
     animal = Animal(name="Juan", sex=Sex.male)
 
