0653bf82d0b5ffaf2d5b48f8079ac42287b46ac4
Rafael Moret <rafaelmoretgalan@gmail.com>
2024-07-01 20:39:36 +0200

    feat: add volitale database init data

--- backend/proteapp/api/deps.py
+++ backend/proteapp/api/deps.py
@@ -1,6 +1,12 @@
 from sqlmodel import create_engine, SQLModel, Session
+from sqlmodel.pool import StaticPool
 
-engine = create_engine("sqlite:///database.db", echo=True)
+engine = create_engine(
+    "sqlite://",
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
 SQLModel.metadata.create_all(engine)
 
 
