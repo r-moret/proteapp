953c30e044dbd8753fa3eee5ece6bb2d3378a1a8
Rafael Moret <rafaelmoretgalan@gmail.com>
2024-07-16 19:20:38 +0200

    feat: add docker exception control

--- backend/proteapp/__main__.py
+++ backend/proteapp/__main__.py
@@ -1,14 +1,21 @@
 import uvicorn
 from typing import Literal
 import docker
+from docker.errors import DockerException
 
 
 def start(mode: Literal["dev", "prd"] = "dev"):
     if mode == "dev":
-        docker_client = docker.from_env()
-        container = docker_client.containers.run(
-            "mongo", ports={"27017": 27017}, auto_remove=True, detach=True
-        )
+        try:
+            docker_client = docker.from_env()
+            container = docker_client.containers.run(
+                "mongo", ports={"27017": 27017}, auto_remove=True, detach=True
+            )
+        except DockerException:
+            print(
+                "Something happening while connecting to Docker. Do you have the Docker daemon initialized?"
+            )
+            return
 
         uvicorn.run("proteapp.api.main:app", host="0.0.0.0", port=8000, reload=mode == "dev")
         container.remove(force=True)
